7a9097fa47c4989d0e4f16756b82c16095e3cec9
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2024-08-01 21:31:33 +0530

    login added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ExpenseForm from './components/add';
+import Login from './components/login';
+
+
 
 function App() {
   return (
-    <ExpenseForm />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={<Login  />} />
+        <Route path="/dash" component={<ExpenseForm />} />
+      </Switch>
+    </Router>
   );
 }
 
